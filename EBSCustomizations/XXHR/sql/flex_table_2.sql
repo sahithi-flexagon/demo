0b826789045c9659ef8053620a705daaa681b6e7
Sahithi-Flexagon <119493731+Sahithi-Flexagon@users.noreply.github.com>
2024-09-09 12:57:57 +0530

    Update flex_table_2.sql

--- EBSCustomizations/XXHR/sql/flex_table_2.sql
+++ EBSCustomizations/XXHR/sql/flex_table_2.sql
@@ -1,4 +1,12 @@
-/
+CREATE PROCEDURE sp_Fail_DivideByZero
+AS
+BEGIN
+    DECLARE @Number INT = 10;
+    DECLARE @Zero INT = 0;
+    -- This will cause a division by zero error
+    SELECT @Number / @Zero AS Result;
+END;
+GO
 -- create table flex_table_2 (
 --  ID int,
 --  NAME varchar(255)
