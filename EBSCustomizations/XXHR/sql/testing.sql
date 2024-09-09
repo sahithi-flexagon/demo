78f7d97cd169d20c907428446c6a2a6b4149391d
Sahithi-Flexagon <119493731+Sahithi-Flexagon@users.noreply.github.com>
2024-09-09 12:51:19 +0530

    Update testing.sql

--- EBSCustomizations/XXHR/sql/testing.sql
+++ EBSCustomizations/XXHR/sql/testing.sql
@@ -1,1 +1,9 @@
-select/
+CREATE PROCEDURE sp_Fail_DivideByZero
+AS
+BEGIN
+    DECLARE @Number INT = 10;
+    DECLARE @Zero INT = 0;
+    -- This will cause a division by zero error
+    SELECT @Number / @Zero AS Result;
+END;
+GO
